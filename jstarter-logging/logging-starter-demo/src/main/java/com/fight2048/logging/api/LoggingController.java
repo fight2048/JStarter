db3e255b4516c08018a4427c3d7ca7e9ef2cbde7
leguang <langmanleguang@gmail.com>
2022-02-07 04:20:08 +0800

    完成logging模块的集成

--- jstarter-logging/logging-starter-demo/src/main/java/com/fight2048/logging/api/LoggingController.java
+++ jstarter-logging/logging-starter-demo/src/main/java/com/fight2048/logging/api/LoggingController.java
@@ -1,6 +1,10 @@
 package com.fight2048.logging.api;
 
 import com.fight2048.logging.Logger;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @description:
  */
 @RestController
+@Api(value = "账号管理", tags = {"账号管理"})
+//@Tag(name = "类上的Tag")
 public class LoggingController {
 
+//    @Tag(name = "方法上的Tag")
+//    @Operation(summary = "获取全部机构信息(树结构)")
     @Logger(value = "我我我")
+    @ApiOperation(value = "登录：通过code进行验证登录", notes = "https://connect.turingthink.com/?redirect_uri=http://127.0.0.1/kf12oi&appId=atcfu9ct2k8x&scope=all&response_type=code", httpMethod = "POST")
     @GetMapping("/logs")
     public Object logs() {
         return "OK";
